65d562344f6f92f7c09f82c6ddac87b06ae39d96
Aviles Santos <maa0866@cell16-cse.unt.ad.unt.edu>
2026-03-20 12:27:52 -0500

    Node.h Updated

--- Node.h
+++ Node.h
@@ -1,11 +1,27 @@
 #ifndef NODE_H
 #define NODE_H
 
+#pragma once
+
+#include "Driver.h"
+
 #include <iostream>
 #include <iomanip>
 #include <cmath>
 
 using namespace std;
 
+class LinkedList;
+
+class Node {
+    private:
+        Driver* data;
+        Node* next;
+
+    public:
+        Node(Driver* driver);
+
+        friend class LinkedList;
+};
 
 #endif
